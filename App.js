75b43b72fbd5138c5de727110bfeb1a30eb02dd4
Jobin Johnson <jobinjohnson01334@gmail.com>
2021-09-14 22:24:16 +0530

    AI News Application

---
+++ App.js
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from 'react';
+import { Typography } from '@material-ui/core';
+import wordsToNumbers from 'words-to-numbers';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+import logo from './components/images/alanlogo.jpg';
+import NewsCards from './components/NewsCards/NewsCards';
+import Modal from './components/Modal/Modal';
+import useStyles from './styles';
+
+const App = () => {
+  const [activeArticle, setActiveArticle] = useState(0);
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    alanBtn({
+      key: '2f8238586cbc82d46bfaca791019a10b2e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: ({ command, articles, number }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === 'instructions') {
+          setIsOpen(true);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === 'open') {
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText('Please try that again...');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          } else {
+            alanBtn().playText('Please try that again...');
+          }
+        }
+      },
+    });
+  }, []);
+
+  return (
+    <div>
+      <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}>
+              <Typography variant='h7' component='h2'>
+                Try saying: <br />
+                <br />
+                Open article number [4]
+              </Typography>
+            </div>
+            <div className={classes.card}>
+              <Typography variant='h7' component='h2'>
+                Try saying: <br />
+                <br />
+                Go back
+              </Typography>
+            </div>
+          </div>
+        ) : null}
+        <img src={logo} className={classes.alanLogo} alt='logo' />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+      {!newsArticles.length ? (
+        <div className={classes.footer}>
+          <Typography variant='body1' component='h2'>
+            Created by
+            <a
+              className={classes.link}
+              href='https://www.linkedin.com/in/jobin-johnson-01334/'
+            >
+              {' '}
+              Jobin Johnson
+            </a>{' '}
+          </Typography>
+          <img
+            className={classes.image}
+            src={logo}
+            height='50px'
+            alt='JSMastery logo'
+          />
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default App;
